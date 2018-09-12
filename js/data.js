2bd4008259cef04b5d200c99956876fc59964170
vagrant <vagrant@vagrant.vm>
2018-09-12 02:50:54 +0000

    first commit

---
+++ js/data.js
@@ -0,0 +1,32 @@
+var MARKERS = [
+	{name:"Shekou International School", 
+position: {lat:22.482458, lng:113.911649},
+wikiname:"Shekou_International_School"
+},
+	{name:"QSI International School", 
+position: {lat:22.481323, lng:113.912995},
+wikiname:"QSI_International_School_of_Shenzhen"
+},
+	{name:"KIS International School", 
+position: {lat:22.497358, lng:113.918127},
+wikiname:"Korean_International_School_in_Shenzhen"
+},
+	{name:"Shenzhen American International School",
+position: {lat:22.571329, lng:114.063868},
+wikiname:"Shenzhen_American_International_School"
+},
+	{name:"Shen Wai International School", 
+position: {lat:22.547956, lng:114.093636},
+wikiname:"Shen_Wai_International_School"
+},
+	{name:"International School of Nanshan Shenzhen", 
+position: {lat:22.561539, lng:113.98628},
+wikiname:"International_School_of_Nanshan_Shenzhen"
+},
+	{name:"Shenzhen Japanese School", 
+position: {lat:22.501345, lng:113.917554},
+wikiname:"Shenzhen_Japanese_School"
+}
+];
+
+var CURRENT_LOCATION = {lat: 22.561539, lng: 113.98628};
